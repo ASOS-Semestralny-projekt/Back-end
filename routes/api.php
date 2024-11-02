31c952277b88a541f5e6f8269d9b2b4469715cd7
richard <korosi.richard@gmail.com>
2024-11-02 19:57:30 +0100

    feat: create UserController for testing the API

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/users', [UserController::class, 'index'])
+        ->name('users.index');
 });
