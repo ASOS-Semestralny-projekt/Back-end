912fc5fdf642f3facfa275ad2356550f44ed6f34
richard <korosi.richard@gmail.com>
2024-11-03 15:50:03 +0100

    feat: combine Auth with the basic API requests

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/users', [UserController::class, 'index'])
-        ->name('users.index');
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::get('/products/{categoryId}', [ProductController::class, 'getByCategory']);
+    Route::get('/product/{productId}', [ProductController::class, 'getById']);
 });
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,4 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProductController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/products/{categoryId}', [ProductController::class, 'getByCategory']);
-Route::get('/product/{productId}', [ProductController::class, 'getById']);
+require __DIR__ . '/auth.php';
+require __DIR__ . '/api.php';
