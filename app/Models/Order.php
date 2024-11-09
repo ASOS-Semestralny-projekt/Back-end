ab1dcffea23338ebc35e382589aefc6da9812342
AdelaRadicova <xradicova@stuba.sk>
2024-11-09 10:09:28 +0100

    fix: create method for order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -19,6 +19,26 @@ class Order extends Model
         'total_price',
     ];
 
+    public static function create(array $attributes)
+    {
+        $order = new self();
+        $order->user_id = $attributes['user_id'];
+        $order->order_number = $attributes['order_number'];
+        $order->order_date_created = $attributes['order_date_created'];
+        $order->total_price = $attributes['total_price'];
+        $order->save();
+
+        if (isset($attributes['products'])) {
+            foreach ($attributes['products'] as $product) {
+                $order->products()->attach($product['id'], [
+                    'quantity' => $product['quantity'],
+                    'price' => $product['price'],
+                ]);
+            }
+        }
+        return $order;
+    }
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
