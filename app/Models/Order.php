efb77691977df42394be7de400426953fe75ac1f
richard <korosi.richard@gmail.com>
2024-11-09 18:41:10 +0100

    fix: correct merge of the Order.php and Product.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,13 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use \Illuminate\Database\Eloquent\Relations\BelongsTo;
-use \Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Order extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'user_id',
         'order_number',
@@ -19,18 +24,54 @@ class Order extends Model
         'total_price',
     ];
 
-    public static function truncate(): void
+    /**
+     * Create a new order.
+     *
+     * @param array $attributes
+     * @return Order
+     */
+    public static function create(array $attributes): Order
     {
-        static::query()->delete();
+        $order = new self();
+        $order->user_id = $attributes['user_id'];
+        $order->order_number = $attributes['order_number'];
+        $order->order_date_created = $attributes['order_date_created'];
+        $order->total_price = $attributes['total_price'];
+        $order->save();
+
+        if (isset($attributes['products'])) {
+            foreach ($attributes['products'] as $product) {
+                $order->products()->attach($product['id'], [
+                    'quantity' => $product['quantity'],
+                    'price' => $product['price'],
+                ]);
+            }
+        }
+        return $order;
     }
 
+    /**
+     * Get the user that the order belongs to.
+     *
+     * @return BelongsTo
+     */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * Get the products that belong to the order.
+     *
+     * @return BelongsToMany
+     */
     public function products(): BelongsToMany
     {
         return $this->belongsToMany(Product::class)->withPivot('quantity', 'price');
+    }
+
+    public static function truncate(): void
+    {
+        static::query()->delete();
     }
 }
